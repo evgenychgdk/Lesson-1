a6f46ec796af008f6f3c6316a8c7eaef53138de5
Evgeny <chahaida07@gmail.com>
2024-02-10 12:40:32 +0200

    000

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Завдання 1")
+print1 = int(input("Напиши першу цифру:"))
+print2 = int(input("Напиши другу цифру:"))
+print3 = int(input("Напиши другу цифру"))
+result1 = print1+print2+print3
+print("Сума:")
+print(result1)
+result2 = print1*print2*print3
+print("Добуток:")
+print(result2)
+
+print("Завдання 2")
+print4 = int(input("Напиши першу діагональ:"))
+
+print5 = int(input("Напиши другу діагональ:"))
+result3 = print4 * print5/2
+print("Площа")
+print(result3)
+
+print("Закрити програму")
+
+
+
+
+
+
+
+
+
+
