5db25093d8990fa392088febfc5ca3a885e0b9a6
Evgeny <chahaida07@gmail.com>
2024-02-13 21:41:33 +0200

    Завдання 1

--- Урок 2.py
+++ Урок 2.py
@@ -1,5 +1,30 @@
-n2, n3 = 10, 30
-print(n2 < n3)
+
+numbers = input('Введыть трьох значне число: ')
+
+
+if numbers[0] >= numbers[1] and numbers[0] >= numbers[2]:
+    print('Max:', numbers[0])
+elif numbers[1] > numbers[0] and numbers[1] > numbers[2]:
+    print('Max: ', numbers[1])
+elif numbers[2] > numbers[0] and numbers[2] > numbers[1]:
+    print('Max: ', numbers[2])
+else:
+    print('Numbers =')
+
+if numbers[0] <= numbers[1] and numbers[0] <= numbers[2]:
+    print('Min:', numbers[0])
+elif numbers[1] <= numbers[0] and numbers[1] <= numbers[2]:
+    print('Min: ', numbers[1])
+elif numbers[2] <= numbers[0] and numbers[2] <= numbers[1]:
+    print('Min: ', numbers[2])
+else:
+    print('Вони рівні')
+sum_numlbers = int(numbers[0]) + int(numbers[1]) + int(numbers[2])
+print('Середнє арифметичне: ', int(sum_numlbers) // 3)
+
+
+
+
 
 
 
