81537f74a9a1eb95a5509977637f9ad344bff8ee
Evgeny <chahaida07@gmail.com>
2024-02-14 23:59:46 +0200

    Завдання 2

--- Урок 3.py
+++ Урок 3.py
@@ -1,24 +1,31 @@
+# #
+# #
+# pruf = int(input("Введіть число:"))
+# if pruf == 1:
+#     print("Monday")
+# elif pruf == 2:
+#     print("Tuesday")
+# elif pruf == 3:
+#     print("Wednesday")
+# elif pruf == 4:
+#     print("Thursday")
+# elif pruf == 5:
+#     print("Friday")
+# elif pruf == 6:
+#     print("Saturday")
+# elif pruf == 7:
+#     print("Sunday")
+# else:
+#     print("None day")
+#
+pruf1 = int(input("Enter first number:"))
+pruf2 = int(input("Enter second number:"))
 
-
-pruf = int(input("Введіть число:"))
-if pruf == 1:
-    print("Monday")
-elif pruf == 2:
-    print("Tuesday")
-elif pruf == 3:
-    print("Wednesday")
-elif pruf == 4:
-    print("Thursday")
-elif pruf == 5:
-    print("Friday")
-elif pruf == 6:
-    print("Saturday")
-elif pruf == 7:
-    print("Sunday")
+if pruf1 == pruf2:
+    print("Numberі are equal to")
+elif pruf1 > pruf2:
+    print(pruf2,',',pruf1)
+elif pruf1 < pruf2:
+    print(pruf1,",",pruf2)
 else:
-    print("None day")
-
-
-
-
-
+    print("WTF")
