318766228cd8b8f0581fdbf0d284f7d26a92b90b
Evgeny <chahaida07@gmail.com>
2024-02-14 23:33:28 +0200

    Завдання 1

---
+++ Урок 3.py
@@ -0,0 +1,24 @@
+
+
+pruf = int(input("Введіть число:"))
+if pruf == 1:
+    print("Monday")
+elif pruf == 2:
+    print("Tuesday")
+elif pruf == 3:
+    print("Wednesday")
+elif pruf == 4:
+    print("Thursday")
+elif pruf == 5:
+    print("Friday")
+elif pruf == 6:
+    print("Saturday")
+elif pruf == 7:
+    print("Sunday")
+else:
+    print("None day")
+
+
+
+
+
