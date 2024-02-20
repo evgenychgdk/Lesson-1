a3e32275878713246ec23466b184e3e6fd794e40
Evgeny <chahaida07@gmail.com>
2024-02-20 11:34:37 +0200

    Завдання

---
+++ Урок 4.py
@@ -0,0 +1,134 @@
+# # "Task1"
+# user_input = input("Введіть рядок: ")
+#
+# letter_count = 0
+# digit_count = 0
+#
+# for char in user_input:
+#     if char.isalpha():
+#         letter_count += 1
+#     elif char.isdigit():
+#         digit_count += 1
+#
+#
+# print(f"Кількість літер: {letter_count}")
+# print(f"Кількість цифр: {digit_count}")
+
+# "Task 2"
+# user_input = input("Введіть рядок: ")
+# symbol = input("Введіть символ для пошуку: ")
+#
+#
+# count = user_input.count(symbol)
+#
+#
+# print(f"Символ '{symbol}' зустрічається {count} разів у введеному рядку.")
+
+
+# "Task 3"
+# string = input("Введіть рядок: ")
+# search_word = input("Введіть слово для пошуку: ")
+# replace_word = input("Введіть слово для заміни: ")
+#
+# new_string = string . replace(search_word, replace_word)
+#
+# print("Оновлений рядок: ", new_string)
+
+
+# "Task 4"
+# try:
+#     user_string = input("Введіть рядок: ")
+#
+#
+#     print(user_string[2])
+#
+#     print(user_string[-2])
+#
+#     print(user_string[:5])
+#
+#     print(user_string[:-2])
+#
+#     print(user_string[::2])
+#
+#     print(user_string[1::2])
+#
+#     print(user_string[::-1])
+#
+#     print(user_string[::-2])
+#
+#     print(len(user_string))
+#
+# except IndexError:
+#     print("Помилка: Рядок недостатньої довжини.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
